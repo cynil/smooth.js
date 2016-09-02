051c66ddbd00247765bbceda69328b180555b9d7
lianjian zhou <lianjian zhou>
2016-09-02 13:27:56 +0800

    git add -A git commit -m "initial commit"

---
+++ src/main.js
@@ -0,0 +1,24 @@
+require.config({
+    paths: {
+        'hammer': '../bower_components/hammerjs/hammer.min',
+        'zepto': '../bower_components/zepto/zepto.min',
+        'ppt': '../lib/ppt'
+    },
+    shim: {
+        'zepto': {
+            exports: 'Zepto'
+        }
+    }
+})
+
+require(['ppt'], function(PPT){
+
+    var ppt = document.querySelector('.ppt')
+    var app = new PPT(ppt)
+    
+    PPT.utils.hm('#back').on('press', function(event){
+        app.load(app.stages[0])
+    })
+
+//end require
+})
